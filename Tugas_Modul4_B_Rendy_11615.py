d7065fb9a142b06a7bd2d4fd09e4e04dae468c05
rendyputrayana <220711615@students.uajy.ac.id>
2024-11-06 15:10:03 +0700

    Update Tugas_Modul4_B_Rendy_11615.py

--- Tugas_Modul4_B_Rendy_11615.py
+++ Tugas_Modul4_B_Rendy_11615.py
@@ -34,7 +34,7 @@ def scatter(model, model_name, data, new_point, features, color_scale, title):
     return fig, new_cluster
 
 st.set_page_config(
-    page_title="XXXXX - Unsupervised Learning", 
+    page_title="11615 - Unsupervised Learning", 
     page_icon="📊",
     layout="wide",
     initial_sidebar_state="expanded",
@@ -44,7 +44,7 @@ uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["cs
 
 if uploaded_file is not None:
     input_data = pd.read_csv(uploaded_file)
-    st.markdown("<h1 style='text-align: center;'>Unsupervised Learning - YYYYY</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>Unsupervised Learning - 11615</h1>", unsafe_allow_html=True)
     st.dataframe(input_data)
     
     model_path = {
